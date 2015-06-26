cb75710c0be7d615d657309e8c6d811ed024d221
Jorge Iván Andrés Contreras Pereira <jorge@contreras.com.co>
2015-06-26 08:47:04 -0500

    Add AsyncTask

---
+++ app/src/main/java/co/edu/unal/clima/FetchWeatherTask.java
@@ -0,0 +1,13 @@
+package co.edu.unal.clima;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by jiacontrerasp on 6/26/15.
+ */
+public class FetchWeatherTask extends AsyncTask<String, Void, Void> {
+    @Override
+    protected Void doInBackground(String... strings) {
+        return null;
+    }
+}
