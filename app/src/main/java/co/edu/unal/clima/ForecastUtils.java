31edfb5e1fb2312bc049afcaf3368d6a5a97f74c
Jorge Iván Andrés Contreras Pereira <jorge@contreras.com.co>
2015-06-26 07:19:35 -0500

    add new classes

---
+++ app/src/main/java/co/edu/unal/clima/ForecastUtils.java
@@ -0,0 +1,21 @@
+package co.edu.unal.clima;
+
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Date;
+import java.util.Vector;
+
+/**
+ * Created by jiacontrerasp on 6/26/15.
+ */
+public class ForecastUtils {
+
+}
---
+++ app/src/main/java/co/edu/unal/clima/Weather.java
@@ -0,0 +1,21 @@
+package co.edu.unal.clima;
+
+/**
+ * Created by jiacontrerasp on 6/26/15.
+ */
+public class Weather {
+
+    private Long locationID;
+    private String date;
+    private double pressure;
+    private int humidity;
+    private double windSpeed;
+    private double windDirection;
+
+    private double high;
+    private double low;
+
+    private String description;
+    private int weatherId;
+
+}
